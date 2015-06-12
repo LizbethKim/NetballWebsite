949d76df7da185fc43ed3c5b34c0740d689b2c99
Pauline Kelly <pauline.m.kelly1@gmail.com>
2015-06-12 14:57:36 +1200

    finished csv parser, should accomodate a 2014 file

--- src/CsvParser.java
+++ src/CsvParser.java
@@ -10,11 +10,17 @@ import java.io.Writer;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
-public class CsvParser {
-	
-	//private Pattern numPattern = new Pattern("\\d", 0);
+/**
+ * A parser to process files that were given out in the SWEN303 d3.js assignment. 
+ * Converts csv files into a format that is uniform between files, and easier to process.
+ * 
+ * @author Pauline Kelly
+ */
+public class Parser {
 
 	private String [] csvFiles = {
+			"2008-Table1.csv",
+			"2009-Table1.csv",
 			"2010-Table1.csv",
 			"2011-Table1.csv",
 			"2012-Table1.csv",
@@ -22,62 +28,42 @@ public class CsvParser {
 			"2014-Table1.csv"
 	};
 
-	public CsvParser(){
-		//process2008results();
-		//process2009results();
-		processOtherResults();
+	/**
+	 * Constructor
+	 */
+	public Parser(){
+		processResults();
 	}
 
-	private void processOtherResults() {
+	/**
+	 * Process the results depending on the year
+	 */
+	public void processResults(){
 		for(String file : csvFiles){
-			String line = "";
-
 			try {
 				Scanner scanner = new Scanner(new File(file));
+
+				//Write to the file depending on the specifics of the file
 				try {
-					Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("new" + file)));
+					Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("converted"+file)));
+
 					//Throw away header information
 					scanner.nextLine();
-					String header = "Round,Date,Home Team,Score,Away Team,Venue\n";
+					String header = "Round,Date,HomeTeam,HomeTeamScore,AwayTeamScore,AwayTeam,Venue\n";
 					writer.write(header);
 
-					while(scanner.hasNextLine()){	
-						line = scanner.nextLine();
-						Scanner lineScanner = new Scanner(line);
-						lineScanner.useDelimiter(",");
-
-						String round = lineScanner.next();
-
-						String dateOrBye = lineScanner.next();
-
-						if(dateOrBye.startsWith("BYES: ")){
-							String [] teams = dateOrBye.split(" and ");
-							String teamA = teams[0];
-							String teamB = teams[1];
-							String newTeamA = teamA.replace("BYES: ", "");
-							line = String.format("BYE,%s,%s,%s",round, newTeamA, teamB);
-						}
-						else {
-							
-							String homeTeam = lineScanner.next();
-							String oldScore = lineScanner.next();
-							String awayTeam = lineScanner.next();
-							String oldVenue = lineScanner.next();
-							//oldVenue = oldVenue.concat(lineScanner.next());
-
-							//Mutation Stage
-							dateOrBye = dateOrBye.matches("\\d");
-							String date = dateOrBye.replace("\"", "");
-							String score = oldScore.replace("–", "-");	 //–
-							String venue = oldVenue.replace("\"", "");
-
-							//Output stage
-							line = String.format("%s,%s,%s,%s,%s,%s", round, date, homeTeam, score, awayTeam, venue);
-						}
-						System.out.println(line);
-						writer.write(line + "\n");
-						lineScanner.close();
+					if(file.contains("2008")){
+						parse2008(scanner, writer);
 					}
+					else if(file.contains("2009")){
+						parse2009(scanner, writer);
+						System.out.println("finished 2009");
+					}
+					else {
+						parseOther(scanner,writer);
+						System.out.println("finished" + file);
+					}
+					System.out.println("wrote to file");
 					writer.close();
 
 				} catch (IOException e) {
@@ -86,131 +72,207 @@ public class CsvParser {
 				scanner.close();
 			}
 			catch(FileNotFoundException e){
-				System.err.println("" + e.getMessage());
-			}
-		}
-	}
-
-	private void process2009results() {
-		String line = "";
-
-		try {
-			Scanner scanner = new Scanner(new File("2009-Table1.csv"));
-			try {
-				Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("v1-2009-Table1.csv")));
-				//Throw away header information
-				scanner.nextLine();
-				String header = "Round,Date,Home Team,Score,Away Team,Venue\n";
-				writer.write(header);
-
-				while(scanner.hasNextLine()){	
-					line = scanner.nextLine();
-					Scanner lineScanner = new Scanner(line);
-					lineScanner.useDelimiter(",");
-
-					String round = lineScanner.next();
-
-					String dateOrBye = lineScanner.next();
-
-					if(dateOrBye.startsWith("BYES: ")){
-						String [] teams = dateOrBye.split(" and ");
-						String teamA = teams[0];
-						String teamB = teams[1];
-						String newTeamA = teamA.replace("BYES: ", "");
-						line = String.format("BYE,%s,%s,%s",round, newTeamA, teamB);
-					}
-					else {
-						lineScanner.next();	//throw away time
-						String homeTeam = lineScanner.next();
-						String oldScore = lineScanner.next();
-						String awayTeam = lineScanner.next();
-						String oldVenue = lineScanner.next();
-						//oldVenue = oldVenue.concat(lineScanner.next());
-
-						//Mutation Stage
-						String date = dateOrBye.replace("\"", "");
-						String score = oldScore.replace("–", "-");	 //–
-						String venue = oldVenue.replace("\"", "");
-
-						//Output stage
-						line = String.format("%s,%s,%s,%s,%s,%s", round, date, homeTeam, score, awayTeam, venue);
-					}
-					System.out.println(line);
-					writer.write(line + "\n");
-					lineScanner.close();
+				if(e.getMessage().startsWith("2014")){
+					System.out.println("2014 not present.");
+					return;
 				}
-				writer.close();
-
-			} catch (IOException e) {
-				System.err.println("Was not able to write to file: " + e.getMessage());
-			}
-			scanner.close();
-		}
-		catch(FileNotFoundException e){
-			System.err.println("" + e.getMessage());
-		}
-	}
-
-	public void process2008results(){
-		String line = "";
-
-		try {
-			Scanner scanner = new Scanner(new File("2008-Table1.csv"));
-			try {
-				Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("v1-2008-Table1.csv")));
-				//Throw away header information
-				scanner.nextLine();
-				String header = "Round,Date,Home Team,Score,Away Team,Venue\n";
-				writer.write(header);
-
-				while(scanner.hasNextLine()){	
-					line = scanner.nextLine();
-					Scanner lineScanner = new Scanner(line);
-					lineScanner.useDelimiter(",");
-
-					String round = lineScanner.next();
-
-					String dateOrBye = lineScanner.next();
-
-					if(dateOrBye.startsWith("BYES: ")){
-						String [] teams = dateOrBye.split(" and ");
-						String teamA = teams[0];
-						String teamB = teams[1];
-						String newTeamA = teamA.replace("BYES: ", "");
-						line = String.format("BYE,%s,%s,%s",round, newTeamA, teamB);
-					}
-					else {
-						String date = dateOrBye;
-						lineScanner.next();							//skip the time, throw away
-						String homeTeam = lineScanner.next();
-						String oldScore = lineScanner.next();
-						String awayTeam = lineScanner.next();
-						String oldVenue = lineScanner.next();
-
-						//Mutation Stage
-						String score = oldScore.replace("–", "-");	 //–
-						String venue = oldVenue.replace("\"", "");
-
-						//Output stage
-						line = String.format("%s,%s,%s,%s,%s,%s", round, date, homeTeam, score, awayTeam, venue);
-					}
-					writer.write(line + "\n");
+				else {
+					System.err.println("" + e.getMessage());
 				}
-				writer.close();
-
-			} catch (IOException e) {
-				System.err.println("Was not able to write to file: " + e.getMessage());
 			}
-			scanner.close();
 		}
-		catch(FileNotFoundException e){
-			System.err.println("" + e.getMessage());
+	}
+
+	/**
+	 * Parse 2008 file, different format than the others
+	 * @param scanner
+	 * @param writer
+	 */
+	private void parse2008(Scanner scanner, Writer writer) {
+		String line = "";
+		while(scanner.hasNextLine()){	
+			line = scanner.nextLine();
+			Scanner lineScanner = new Scanner(line);
+			lineScanner.useDelimiter(",");
+
+			String round = lineScanner.next();
+			
+			String dateOrBye = lineScanner.next();
+			
+			if(dateOrBye.startsWith("BYES: ")){
+				String [] teams = dateOrBye.split(" and ");
+				String teamA = teams[0];
+				String teamB = teams[1];
+				String newTeamA = teamA.replace("BYES: ", "");
+				line = String.format("BYE,%s,%s,%s",round, newTeamA, teamB);
+			}
+			else {
+				lineScanner.next();							//skip the time, throw away
+				String homeTeam = lineScanner.next();
+				String oldScore = lineScanner.next();
+				
+				//System.out.println("preScore " + oldScore);
+				String awayTeam = lineScanner.next();
+				String oldVenue = lineScanner.next();
+				//String city = lineScanner.next();  //can't do city! too hard to extract from strings
+
+				//Mutation Stage
+				//Dates
+				String [] dateInfo = dateOrBye.split(" ");
+				String day = dateInfo[0];
+				String dayNumber = dateInfo[1];
+				String month = dateInfo[2];
+								
+				//Scores
+				String[] score = oldScore.split("â€“|-");	
+				
+				String homeScore = score[0];
+				String awayScore = score[1];
+								
+				String venue = oldVenue.replace("\"", "");
+				
+				//Output stage
+				line = String.format("%s,%s,%s,%s,%s,%s,%s,%s,%s", round, day, dayNumber, month, homeTeam, homeScore, awayScore, awayTeam, venue);
+			}
+			try {
+				writer.write(line + "\n");
+			} catch (IOException e) {
+				e.printStackTrace();
+				throw new RuntimeException("Couldn't print the line");
+			}
+		}
+	}
+
+	/**
+	 * Parse the 2009 games. The date is in a different format to the others.
+	 * @param scanner
+	 * @param writer
+	 */
+	private void parse2009(Scanner scanner, Writer writer) {
+		String line = "";
+		while(scanner.hasNextLine()){	
+			line = scanner.nextLine();
+			Scanner lineScanner = new Scanner(line);
+			lineScanner.useDelimiter(",");
+
+			String round = lineScanner.next();
+
+			String dateOrBye = lineScanner.next();
+
+			if(dateOrBye.startsWith("BYES: ")){
+				String [] teams = dateOrBye.split(" and ");
+				String teamA = teams[0];
+				String teamB = teams[1];
+				String newTeamA = teamA.replace("BYES: ", "");
+				line = String.format("BYE,%s,%s,%s",round, newTeamA, teamB);
+			}
+			else {
+				lineScanner.next();	//throw away time
+				String homeTeam = lineScanner.next();
+				//System.out.println("Hometeam: " + homeTeam);
+				String oldScore = lineScanner.next();
+				String awayTeam = lineScanner.next();
+				String oldVenue = lineScanner.next();
+				//oldVenue = oldVenue.concat(lineScanner.next());
+
+				//Mutation Stage
+				//Dates				
+				String [] dateInfo = dateOrBye.split(" ");
+				String day = dateInfo[0].replace("\"", "");;
+				String dayNumber = dateInfo[1];
+				String month = dateInfo[2];
+				
+				//Scores
+				//System.out.println("Round: " + round + ", Oldscore " + oldScore);
+				String[] score = oldScore.split("â€“|-");	
+				
+				String homeScore = score[0];
+				String awayScore = score[1];
+				
+				String venue = oldVenue.replace("\"", "");
+
+				//Output stage
+				line = String.format("%s,%s,%s,%s,%s,%s,%s,%s,%s", round, day, dayNumber, month, homeTeam, homeScore, awayScore, awayTeam, venue);
+			}
+			try {
+				writer.write(line + "\n");
+			} catch (IOException e) {
+				e.printStackTrace();
+				throw new RuntimeException("Couldn't print the line");
+			}
 		}
 	}
 
 
+	/**
+	 * Parses all other files, as they conform to the same format.
+	 * @param scanner
+	 * @param writer
+	 */
+	private void parseOther(Scanner scanner, Writer writer) {
+		String line = "";
+		while(scanner.hasNextLine()){	
+			line = scanner.nextLine();
+			Scanner lineScanner = new Scanner(line);
+			lineScanner.useDelimiter(",");
+
+			String round = lineScanner.next();
+			//System.out.println("round:" + round);
+
+			String dateOrBye = lineScanner.next();
+			System.out.println("date:" + dateOrBye);
+
+			
+			if(dateOrBye.startsWith("BYES: ")){
+				String [] teams = dateOrBye.split(" and ");
+				String teamA = teams[0];
+				String teamB = teams[1];
+				String newTeamA = teamA.replace("BYES: ", "");
+				line = String.format("BYE,%s,%s,%s",round, newTeamA, teamB);
+			}
+			else {
+				String homeTeam = lineScanner.next();
+				String oldScore = lineScanner.next();
+				String awayTeam = lineScanner.next();
+				String oldVenue = lineScanner.next();
+				//oldVenue = oldVenue.concat(lineScanner.next());
+
+				//Mutation Stage
+				String date = dateOrBye.replace("\"", "");
+				
+				String [] dateInfo = dateOrBye.split(" ");
+				String day = dateInfo[0];
+				String dayNumber = dateInfo[1];
+				String month = dateInfo[2];
+				
+				//Scores
+				oldScore = oldScore.replace(" ", "");
+				String[] score = oldScore.split("â€“|-");	
+				
+				String homeScore = score[0];
+				String awayScore = score[1];
+				
+				String venue = oldVenue.replace("\"", "");
+
+				//Output stage
+				line = String.format("%s,%s,%s,%s,%s,%s,%s,%s,%s", round, day, dayNumber, month, homeTeam, homeScore, awayScore, awayTeam, venue);
+				System.out.println(line);
+			}
+			try {
+				writer.write(line + "\n");
+			} catch (IOException e) {
+				e.printStackTrace();
+				throw new RuntimeException("Couldn't print the line");
+			}
+		}
+	}
+
+	/**
+	 * Main
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		CsvParser csvp = new CsvParser();
+		new Parser();
 	}
 
 }
