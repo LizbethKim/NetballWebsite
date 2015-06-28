b63c86c833ed412c13e4803eb4df0a8f03fbc452
Elizabeth Kim <eliz.kim91@gmail.com>
2015-06-28 22:27:39 +1200

    Fixed up Recent Games

--- js/upcomingMatches.js
+++ js/upcomingMatches.js
@@ -1,7 +1,7 @@
 function populateMatches(tableId){
 	var len = newData.length - 1;
 	var dat = newData[len].data;
-	dat = dat.splice(newData[len].data.length - 8, newData[len].data.length);
+	dat = dat.splice(newData[len].data.length - 7, newData[len].data.length);
 	$.each(dat, function(idx, e){
 		var tr = $('<tr>');
 			$(tr).append($('<td>').text(e.dayNumber + " " + e.month));
@@ -14,12 +14,13 @@ function populateMatches(tableId){
 function populateScores(tableId){
 	var len = newData.length - 1;
 	var dat = newData[len].data;
-	dat = dat.splice(newData[len].data.length - 8, newData[len].data.length);
+	dat = dat.splice(newData[len].data.length - 7, newData[len].data.length);
 	$.each(dat, function(idx, e){
 		var tr = $('<tr>')
 			.append($('<td>').text(e.dayNumber + " " + e.month));
 			$(tr).append($('<td>').text(e.homeTeam));
 			$(tr).append($('<td>').text(e.homeTeamScore));
+			$(tr).append($('<td>').text("-"));
 			$(tr).append($('<td>').text(e.awayTeamScore));
 			$(tr).append($('<td>').text(e.awayTeam));
 			$(tableId).append($(tr));
